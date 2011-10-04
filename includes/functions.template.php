b54a37c62f4bd5201a41c339bd053a50a93e1602
Josh Lawrence <jlawrence@webershandwick.com>
2011-10-04 09:31:08 -0400

    updated paths from /js to /scripts, updated libraries

--- includes/functions.template.php
+++ includes/functions.template.php
@@ -13,26 +13,22 @@ get_template() pulls a template file from the /content folder. Pass a template n
 get_template('sidebar', array('myvar' => 'example'));
 
 */
-function get_template($file, $metas = array(), $echo = true)
-{
+function get_template($file, $metas = array(), $echo = true){
 	global $build;
 	if(!$echo) ob_start();
 	$build->load($file . '.php', $metas);
 	if(!$echo) return ob_get_clean();
 }
 
-function get_header($metas = array())
-{
+function get_header($metas = array()){
 	get_template('header', $metas);
 }
 
-function get_sidebar($metas = array())
-{
+function get_sidebar($metas = array()){
 	get_template('sidebar', $metas);
 }
 
-function get_footer($metas = array())
-{
+function get_footer($metas = array()){
 	get_template('footer', $metas);
 }
 
@@ -43,8 +39,7 @@ Same as the WP function:
 if(is_page('home')) echo 'Homepage!';
 
 */
-function is_page($page)
-{
+function is_page($page){
 	return (meta('body_class', false) == $page) ? true : false;
 }
 
@@ -61,8 +56,7 @@ function meta_title() {
 }
 
 */
-function meta($item, $echo = true)
-{
+function meta($item, $echo = true){
 	global $meta;
 	if(is_callable('meta_' . $item)) $meta[$item] = call_user_func('meta_' . $item);
 	if(isset($meta[$item]) && $echo) echo $meta[$item];
@@ -70,56 +64,46 @@ function meta($item, $echo = true)
 	else return false;
 }
 
-function load_scripts($load = array('all'), $extra = array())
-{
+function load_scripts($load = array('all'), $extra = array()){
 	$scripts = array(
-		'modernizr' => array('url' => meta('resources', false) . '/js/modernizr.js'),
+		'modernizr' => array('url' => meta('resources', false) . '/scripts/modernizr.js'),
 		'jquery' => array('url' => '//ajax.googleapis.com/ajax/libs/jquery/1.6/jquery.min.js'),
 		'swfobject' => array('url' => '//ajax.googleapis.com/ajax/libs/swfobject/2.2/swfobject.js'),
 		'facebook' => array('url' => '//connect.facebook.net/en_US/all.js'),
-		'easing' => array('url' => meta('resources', false) . '/js/easing.js'),
-		'pngfix' => array('url' => meta('resources', false) . '/js/pngfix.js'),
-		'master' => array('url' => meta('resources', false) . '/js/master.js'),
-		'scripts' => array('url' => meta('content', false) . '/js/scripts.js')
+		'easing' => array('url' => meta('resources', false) . '/scripts/easing.js'),
+		'pngfix' => array('url' => meta('resources', false) . '/scripts/pngfix.js'),
+		'master' => array('url' => meta('resources', false) . '/scripts/master.js'),
+		'scripts' => array('url' => meta('content', false) . '/scripts/scripts.js')
 	);
 	array_merge($scripts, $extra);
 	
-	foreach($scripts as $name => $info)
-	{
-		if(in_array('all', $load) || in_array($name, $load)) 
-		{
+	foreach($scripts as $name => $info)	{
+		if(in_array('all', $load) || in_array($name, $load)) {
 			if(isset($info['conditional'])) echo '<!--[if ' . $info['conditional'] . ']>';
 			echo '<script src="' . $info['url'] . '"></script>';
 			if(isset($info['conditional'])) echo '<![endif]-->';
-			echo '
-	';
+			echo '';
 		}
 	}
-	echo '
-';
+	echo '';
 }
 
-function load_styles($load = array('all'), $extra = array())
-{
+function load_styles($load = array('all'), $extra = array()) {
 	$styles = array(
 		'master' => array('url' => meta('resources', false) . '/styles/master.css'),
-		'style' => array('url' => meta('content', false) . '/styles/style.css')
+		'style' => array('url' => meta('content', false) . '/styles/styles.css')
 	);
 	array_merge($styles, $extra);
 	
-	foreach($styles as $name => $info)
-	{
+	foreach($styles as $name => $info){
 		$info['media'] = (isset($info['media'])) ? $info['media'] : 'all';
 		
-		if(in_array('all', $load) || in_array($name, $load))
-		{
+		if(in_array('all', $load) || in_array($name, $load)) {
 			if(isset($info['conditional'])) echo '<!--[if ' . $info['conditional'] . ']>';
 			echo '<link rel="stylesheet" href="' . $info['url'] . '" media="' . $info['media'] . '" />';
 			if(isset($info['conditional'])) echo '<![endif]-->';
-			echo '
-	';
+			echo '';
 		}
 	}
-	echo '
-';
+	echo '';
 }
