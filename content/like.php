854f56dfe90a5cc5153e34cf6cf9d087b0a219fa
Josh Lawrence <jlawrence@webershandwick.com>
2011-11-28 17:41:05 -0500

    updated clearfix to group on like page

--- content/like.php
+++ content/like.php
@@ -1,6 +1,6 @@
 <?php get_header(array('title' => '', 'description' => '', 'keywords' => '')); ?>
 
-	<section id="primary" class="primary clear">
+	<section id="primary" class="primary group">
 		<p>Like this page to continue.</p>
 	</section>
 			
